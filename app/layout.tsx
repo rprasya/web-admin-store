a6d8fa9a1f5b4ecbeb3c409c831f45028b45a33e
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-09-02 13:08:16 +0700

    fix: bugs error void code

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider afterSignOutUrl="/sign-in">
+    <ClerkProvider>
       <html lang="en">
         <body className={inter.className}>
           <ToasterProvider />
