0b4c7dcebb3ef9d6494fa76cc2aee53bb2f11d4e
Rinaldi Prasya <103562307+rprasya@users.noreply.github.com>
2024-09-04 13:56:49 +0700

    To expose an API to the public in the settings form.

--- app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
+++ app/(dashboard)/[storeId]/(routes)/settings/components/settings-form.tsx
@@ -23,6 +23,8 @@ import toast from "react-hot-toast";
 import axios from "axios";
 import { useParams, useRouter } from "next/navigation";
 import { AlertModal } from "@/components/modals/alert-modal";
+import { ApiAlert } from "@/components/ui/api-alert";
+import UseOrigin from "@/hooks/use-origin";
 
 interface SettingsFormProps {
   initialData: Store;
@@ -37,6 +39,7 @@ type SettingsFormValues = z.infer<typeof formSchema>;
 const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
   const params = useParams();
   const router = useRouter();
+  const origin = UseOrigin();
 
   const [open, setOpen] = useState(false);
   const [loading, setLoading] = useState(false);
@@ -123,6 +126,8 @@ const SettingsForm: React.FC<SettingsFormProps> = ({ initialData }) => {
           </Button>
         </form>
       </Form>
+      <Separator />
+      <ApiAlert title="PUBLIC_API_URL" description={`${origin}/api/${params.storeId}`} variant="public" />
     </>
   );
 };
---
+++ hooks/use-origin.tsx
@@ -0,0 +1,21 @@
+import { useEffect, useState } from "react";
+
+const UseOrigin = () => {
+  const [mounted, setMounted] = useState(false);
+  const origin =
+    typeof window !== "undefined" && window.location
+      ? window.location.origin
+      : "";
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return "";
+  }
+
+  return origin;
+};
+
+export default UseOrigin;
